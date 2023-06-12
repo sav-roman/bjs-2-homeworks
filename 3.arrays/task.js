cfae5494e0c737566282a16d8de51fd1ca09556d
Roman Sav <sav.roman@gmail.com>
2023-06-13 00:16:04 +0300

    Task #3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,31 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  return arr1.every((item, index) => {
+    if (item === arr2[index]) {
+        return true;
+    }
+    else {
+        return false;
+    }
+  });
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+      
+  if (users.length === 0) return 0;
+
+  if ( gender !== "мужской" && gender !== "женский" ) return 0;
+
+  let result = users.filter(value => value.gender === gender).map(value => value.age).reduce((acc, item, index, arr) => {
+    acc += item;
+    if (index + 1 === arr.length) {
+      return (acc / arr.length);
+    }
+    return acc;
+    });
+
+    return result;
 }
